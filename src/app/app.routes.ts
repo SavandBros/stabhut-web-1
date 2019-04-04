317384ad662fa3d0ff9ab4340b7a18bd2dea3d21
Amir Savand <amir@savandbros.com>
2019-04-05 00:19:42 +0300

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,28 @@
 import { UIRouter } from '@uirouter/angular';
 import { MainComponent } from './components/main/main.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashComponent } from './components/dash/dash.component';
 
 /**
  * App router routes
  */
-export const Routes: object[] = [{
+export const Routes: any[] = [{
   name: 'main',
-  url: '/',
+  url: '/:id',
   component: MainComponent
+}, {
+  name: 'dash',
+  url: '/dash',
+  component: DashComponent
+}, {
+  name: 'sign-in',
+  url: '/sign-in',
+  component: SignInComponent
+}, {
+  name: 'sign-up',
+  url: '/sign-up',
+  component: SignUpComponent
 }];
 
 /**
