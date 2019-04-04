83760d2a67cfafaddc1bc41ac23ab24b366d916b
Amir Savand <amir@savandbros.com>
2019-04-05 00:20:55 +0300

    Sub to user and show auth links and navs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth/auth.service';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  user: User;
+
+  constructor(public authService: AuthService) {
+  }
+
+  ngOnInit() {
+    this.authService.user.subscribe((data: User) => {
+      this.user = data;
+    });
+  }
 }
