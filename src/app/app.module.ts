147bf146fe945c3d6d8ad4f45ef9d5d86c9274f1
Amir Savand <amir@savandbros.com>
2019-04-05 00:21:45 +0300

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { UIRouterModule } from '@uirouter/angular';
-
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { DashComponent } from './components/dash/dash.component';
 import { Routes, uiRouterConfigFn } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TooltipModule } from 'ngx-bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCog, faUser } from '@fortawesome/free-solid-svg-icons';
-import { TooltipModule } from 'ngx-bootstrap';
+import { ApiService } from './services/api/api.service';
+import { AuthService } from './services/auth/auth.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    SignInComponent,
+    SignUpComponent,
+    DashComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule,
+    ReactiveFormsModule,
     FontAwesomeModule,
     UIRouterModule.forRoot({
       states: Routes,
@@ -28,7 +35,10 @@ import { TooltipModule } from 'ngx-bootstrap';
     }),
     TooltipModule.forRoot(),
   ],
-  providers: [],
+  providers: [
+    ApiService,
+    AuthService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
