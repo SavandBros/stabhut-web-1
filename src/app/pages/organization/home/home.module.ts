c0d1237349e48806aed81cac82d9206156c06616
Amir Savand <amir@savandbros.com>
2019-12-13 15:52:04 +0300

    Add new organization link

--- src/app/pages/organization/home/home.module.ts
+++ src/app/pages/organization/home/home.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
-import { HomeComponent } from 'src/app/pages/organization/home/home.component';
+import { HomeComponent } from './home.component';
 
 const routes: Routes = [{
   path: '',
@@ -16,6 +17,7 @@ const routes: Routes = [{
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    FontAwesomeModule,
   ],
 })
 export class HomeModule {
