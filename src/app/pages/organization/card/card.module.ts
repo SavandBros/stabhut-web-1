1e01095c2b0cb6e2f290f066e56d4d4d4458417d
Arsalan Savand <arsalan@savandbros.com>
2019-12-21 13:25:29 +0300

    Import needed modules

--- src/app/pages/organization/card/card.module.ts
+++ src/app/pages/organization/card/card.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { PopoverModule, TooltipModule } from 'ngx-bootstrap';
 
 import { CardComponent } from 'src/app/pages/organization/card/card.component';
@@ -19,6 +21,8 @@ const routes: Routes = [{
     RouterModule.forChild(routes),
     PopoverModule.forRoot(),
     TooltipModule.forRoot(),
+    ReactiveFormsModule,
+    FontAwesomeModule,
   ],
 })
 export class CardModule {
