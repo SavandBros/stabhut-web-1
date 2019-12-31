ce66dfaa073367ca96e7a5a7eaf5bc464ca6d6a3
Amir Savand <amir@savandbros.com>
2019-12-31 03:42:17 +0300

    Add docs

--- src/app/pages/organization/shared/organization-base.ts
+++ src/app/pages/organization/shared/organization-base.ts
@@ -13,32 +13,48 @@ export class OrganizationBase implements OnInit {
   users: User[];
 
   ngOnInit(): void {
+    // Initialize organization
     OrganizationService.organization.subscribe((data: Organization): void => {
       this.organization = data;
     });
+    // Initialize projects
     OrganizationService.projects.subscribe((data: Project[]): void => {
       this.projects = data;
     });
+    // Initialize labels
     OrganizationService.labels.subscribe((data: Label[]): void => {
       this.labels = data;
     });
+    // Initialize users
     OrganizationService.users.subscribe((data: User[]): void => {
       this.users = data;
     });
   }
 
+  /**
+   * @returns Local project data
+   * @param id Project ID
+   */
   getProject(id: number): Project {
     if (this.projects) {
       return this.projects.find(item => item.id === id);
     }
   }
 
+  /**
+   * @returns Local label data
+   * @param id Label ID
+   */
   getLabel(id: number): Label {
     if (this.labels) {
       return this.labels.find(item => item.id === id);
     }
   }
 
+  /**
+   * @returns Local user data
+   * @param username User username
+   */
   getUser(username: string): User {
     if (this.users) {
       return this.users.find(item => item.username === username);
