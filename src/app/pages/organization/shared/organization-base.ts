19d418367a6abdb982b80a0487f9d6d9a2b3e005
Amir Savand <amir@savandbros.com>
2019-12-31 03:40:28 +0300

    Add getting a single project, label and user

--- src/app/pages/organization/shared/organization-base.ts
+++ src/app/pages/organization/shared/organization-base.ts
@@ -1,17 +1,18 @@
+import { OnInit } from '@angular/core';
 import { Label } from '@app/interfaces/label';
 import { Organization } from '@app/interfaces/organization';
 import { Project } from '@app/interfaces/project';
 import { User } from '@app/interfaces/user';
 import { OrganizationService } from '@app/pages/organization/organization.service';
 
-export class OrganizationBase {
+export class OrganizationBase implements OnInit {
 
   organization: Organization;
   projects: Project[];
-  labels: Project[];
+  labels: Label[];
   users: User[];
 
-  constructor() {
+  ngOnInit(): void {
     OrganizationService.organization.subscribe((data: Organization): void => {
       this.organization = data;
     });
@@ -24,5 +25,23 @@ export class OrganizationBase {
     OrganizationService.users.subscribe((data: User[]): void => {
       this.users = data;
     });
+  }
+
+  getProject(id: number): Project {
+    if (this.projects) {
+      return this.projects.find(item => item.id === id);
+    }
+  }
+
+  getLabel(id: number): Label {
+    if (this.labels) {
+      return this.labels.find(item => item.id === id);
+    }
+  }
+
+  getUser(username: string): User {
+    if (this.users) {
+      return this.users.find(item => item.username === username);
+    }
   }
 }
