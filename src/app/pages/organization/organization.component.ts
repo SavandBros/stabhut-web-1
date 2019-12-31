4d324159baf87551bba1d50bb1d8bf47ef542ea6
Amir Savand <amir@savandbros.com>
2019-12-31 04:19:28 +0300

    Load organization data (needs improvement)

--- src/app/pages/organization/organization.component.ts
+++ src/app/pages/organization/organization.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Organization } from '@app/interfaces/organization';
+import { OrganizationService } from '@app/pages/organization/organization.service';
+import { ApiService } from '@app/services/api.service';
 
 @Component({
   selector: 'app-organization',
   templateUrl: './organization.component.html',
   styleUrls: ['./organization.component.scss'],
 })
-export class OrganizationComponent {
+export class OrganizationComponent implements OnInit {
+
+  constructor(private api: ApiService,
+              private organizationService: OrganizationService) {
+  }
+
+  ngOnInit(): void {
+    this.api.getOrganization(
+      Number(location.pathname.split('/organization/')[1].split('/')[0]),
+    ).subscribe((data: Organization): void => {
+      OrganizationService.currentOrganization = data;
+      this.organizationService.initialise();
+    });
+  }
 }
